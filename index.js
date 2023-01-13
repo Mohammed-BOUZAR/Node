4c764bea4220328657deff0ab8b9947da18a6856
mohammed1.bouzar@gmail.com <mohammed1.bouzar@gmail.com>
2023-01-13 19:32:00 +0000

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-alert("hh");
+const mongoose = require('mongoose');
 
-// success
+mongoose.connect('mongodb://127.0.0.1:27017/playground')
+.then(() => console.log("MongoDB is connected ...."))
+.catch(err => console.error("MongoDB isn't connected ...."));
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: {type: Date, default: Date.now},
+    isPublished: Boolean
+});
+
+const Course = mongoose.model('Course', courseSchema);
+const course = new Course({
+    name: 'NodeJS Course',
+    author: 'Mohammed',
+    tags: ['node', 'backend'],
+    isPublished: true
+});
